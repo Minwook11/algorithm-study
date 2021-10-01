a23d4199a8a47b2f820997b4c8a4b5447c02c4ef
Minwook11 <alsdnr4874@gmail.com>
2021-10-01 22:27:36 +0900

    부족한 금액 계산하기 풀이 중 가장 많은 좋아요 받은 Code 참고용으로 추가

--- programmers/calculate_shortfall.py
+++ programmers/calculate_shortfall.py
@@ -8,3 +8,7 @@ def solution(price, money, count):
     return 0 if (money - result) >=  0 else abs(money - result)
 
 print(solution(3, 20, 4))
+
+# Other programmer's solution -- Most liked solution
+#def solution(price, money, count):
+#    return max(0,price*(count+1)*count//2-money)
