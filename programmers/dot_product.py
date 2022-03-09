e25409245a96817506b3145bc200da183735d4e5
Minwook11 <alsdnr4874@gmail.com>
2022-03-09 22:30:55 +0900

    이동된 파일 커밋 반영

--- programmers/dot_product.py
+++
@@ -1,6 +0,0 @@
-# link : https://programmers.co.kr/learn/courses/30/lessons/70128
-
-def solution(a, b):
-    return sum( [ item1 * item2 for item1, item2 in zip(a, b) ] )
-
-print(solution([1,2,3,4], [-3,-1,0,2]))
--- programmers/n_numbers_spaced_by_x.py
+++
@@ -1,6 +0,0 @@
-# link : https://programmers.co.kr/learn/courses/30/lessons/12954
-
-def solution(x, n):
-    return [ x * i for i in range(1, n + 1) ]
-
-print(solution(-4, 2))
