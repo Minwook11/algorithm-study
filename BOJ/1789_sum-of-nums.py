f2ad0248820203ab9ebfbc27f7bfc07d763a190e
Minwook11 <alsdnr4874@gmail.com>
2022-04-07 09:06:00 +0900

    백준 - 수들의 합(1789) - 해결 못하겠어서 답안 코드까지 확인 진행함 - 접근 방식을 로직화하는 과정을 다시 생각해볼 필요가 있어보인다.

--- BOJ/1789_sum-of-nums.py
+++ BOJ/1789_sum-of-nums.py
@@ -2,13 +2,22 @@
 
 S = int(input())
 
-answer, step = 0, [1]
+step = [1]
 while True:
-    answer += step[-1]
-    if answer >= S:
+    if sum(step) > S:
+        step = step[:-1]
+        break
+    elif sum(step) == S:
         break
     else:
         step.append(step[-1] + 1)
         
-print(answer)
-print(len(step) - 1)
+print(len(step))
+
+
+# 참고한 답안
+# s = int(input())
+# n = 1
+# while n * (n + 1) / 2 <= s:
+#     n += 1
+# print(n - 1)
