f15229c8616ad88134debd1169be63b8026c0375
mwwang <mwwang@jlkgroup.com>
2022-05-25 08:58:09 +0900

    백준 - 나이순 정렬(10814)  - 결국 튜플을 사용하는 방향으로 전환했으나 왜 튜플을 써야하나 싶음  - 튜플 사용 이유에 대해서 확실하게 짚고 넘어가야 할 것 같다.

--- BOJ/10814_sorting-by-age.py
+++ BOJ/10814_sorting-by-age.py
@@ -2,8 +2,9 @@
 
 count = int(input())
 
-users = [ input().split() for _ in range(count) ]
+tuplize = lambda x: (int(x[0]), x[1])
+users = [ tuplize(input().split()) for _ in range(count) ]
 users.sort(key=lambda x: x[0])
 
 for user in users:
-    print(int(user[0]), user[1])
+    print(user[0], user[1])
