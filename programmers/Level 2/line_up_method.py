3e5d5a65a657f0f194576788e5a351524df4f494
Minwook11 <alsdnr4874@gmail.com>
2022-06-01 22:55:40 +0900

    프로그래머스 - 줄 서는 방법  - 최초 시도는 모든 순열 모듈을 통해서 모든 케이스 계산하여 리턴     - 당연히 제한 시간 초과 및 추가 효율성 체크 통과 실패 - 순열의 개수를 계산 후, 순서에 맞게 값을 조정하는 방식으로 접근     - 접근 방식 자체는 괜찮았으나, 로직 구현단계에서 시간이 너무 소모 - 다른 답안 참고해서 진행함     - 접근 방식은 유사하나, 배열 구성 방식이 더 간단하게 구현되었다. - 백준 저레벨에 너무 익숙해져 있었던 것 같다. - 헬스처럼 점진적으로 어려운 문제에 도전해야 할 필요성을 느낀다. - 다만 시간을 좀 여유있게 사용할 수 있는 상황에서 진행할 것

---
+++ programmers/Level 2/line_up_method.py
@@ -0,0 +1,22 @@
+# link : https://programmers.co.kr/learn/courses/30/lessons/12936
+
+import math
+
+def solution(n, k):
+    answer = []
+    numberList = [i for i in range(1, n+1)]
+
+    while (n != 0):
+        temp = math.factorial(n) // n
+        index = k // temp
+        k = k % temp
+        if k == 0:
+            answer.append(numberList.pop(index-1))
+        else :
+             answer.append(numberList.pop(index))
+
+        n -= 1
+    
+    return answer
+
+print(solution(20,1))
