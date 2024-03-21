e90c2307a80a02e045ad801fa83a1a7632c2769f
Minwook11 <alsdnr4874@gmail.com>
2024-03-21 18:58:54 +0900

    프로그래머스 - 야근 지수  - 찹고한 답안 업데이트   - HEAP을 사용하는 방식   - 부호를 반대로 사용하여 MAX HEAP의 형태로 사용됨    - Python의 heapq는 기본적으로 MIN HEAP으로 작동한다고 한다.

--- programmers/Level 3/night_shift_index.py
+++ programmers/Level 3/night_shift_index.py
@@ -1,42 +1,49 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/12927
 
 
+# def solution(n, works):
+#     if len(works) > 1:
+#         while True:
+#             abs_remain_list = sorted(list(set(works)), reverse=True)
+
+#             if len(abs_remain_list) > 1:
+#                 largest, larger = abs_remain_list[0], abs_remain_list[1]
+#                 worked = largest - larger
+#             else:
+#                 largest, larger = abs_remain_list[0], abs_remain_list[0]
+#                 worked = 1
+
+#             if largest == 0:
+#                 break
+
+#             if n >= worked:
+#                 n -= worked
+#                 works[works.index(largest)] -= worked
+#             else:
+#                 works[works.index(largest)] -= n
+
+#             if n == 0:
+#                 break
+
+#         return sum([i**2 for i in works])
+#     else:
+#         remain = works[0] - n if works[0] > n else 0
+
+#         return remain ** 2
+
+import heapq
+
 def solution(n, works):
-    if len(works) > 1:
-        index = 0
-        while True:
-            if index == 50:
-                break
-
-            abs_remain_list = sorted(list(set(works)), reverse=True)
-            print(abs_remain_list)
-            if len(abs_remain_list) > 1:
-                largest, larger = abs_remain_list[0], abs_remain_list[1]
-                worked = largest - larger
-            else:
-                largest, larger = abs_remain_list[0], abs_remain_list[0]
-                worked = 1
-
-            if largest == 0:
-                break
-            print(f"works list : {works}, largest : {largest}, larger : {larger}")
-
-            print(f"left hours : {n}, worked : {worked}")
-            if n >= worked:
-                n -= worked
-                works[works.index(largest)] -= worked
-            else:
-                works[works.index(largest)] -= n
-            print(f"left work : {works}")
-            if n == 0:
-                break
-
-            index += 1
-        print(f"{index} 번 반복")
-        return sum([i**2 for i in works])
-    else:
-        remain = works[0] - n if works[0] > n else 0
-
-        return remain ** 2
+    if n >= sum(works):
+        return 0
+    
+    works = [-w for w in works]
+    heapq.heapify(works)
+    for _ in range(n):
+        i = heapq.heappop(works)
+        i += 1
+        heapq.heappush(works, i)
+    
+    return sum([w ** 2 for w in works])
 
 print(solution(4, [4,3,3]))
