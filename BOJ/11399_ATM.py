c75cfcf425994edef36dfd32f4bff79d725656a4
Minwook11 <alsdnr4874@gmail.com>
2024-04-05 19:09:57 +0900

    백준 - ATM  - 그리디 알고리즘 목표 풀이 진행   - 그리디 방식 자체는 단순하나 다른 부분의 최적화를 신경 쓸 것.  * 파이썬의 map() 함수를 알아볼 필요가 있다.

---
+++ BOJ/11399_ATM.py
@@ -0,0 +1,12 @@
+# link : https://www.acmicpc.net/problem/11399
+
+n = int(input())
+arr = list(map(int,input().split()))
+arr.sort()
+
+result = 0
+
+for i in range(1,n):
+    arr[i] += arr[i-1]
+
+print(sum(arr))
