2b4fabeab4b5a6a7ec1ec13af11dae7a40d25039
Minwook11 <alsdnr4874@gmail.com>
2024-04-09 18:49:54 +0900

    백준 - DNA  - 문제 해석을 다르게 해서, 주어진 문자열들의 차이를 비교   - 문제는 가장 차이가 적은 문자열을 만들어야 함   - 만들어진 문자열과 주어진 문자열의 차이의 합을 출력할 필요도 있음  - 가장 겹치는 횟수가 많은 문자로 문자열 구성   - 나머지 문자들의 등장 횟수를 모두 더하면 차이의 합을 구할 수 있음 * 문제를 차분히 이해해야 한다. 근데 솔직히 문제가 어중간하긴 했음...  그래도 작성한 답변이 한번에 통과된 점은 기분 좋았다.

---
+++ BOJ/1969_DNA.py
@@ -0,0 +1,25 @@
+# link : https://www.acmicpc.net/problem/1969
+
+N, M = list(map(int,input().split()))
+
+pre_define_counter = {index : {"A" : 0, "C" :0, "G" : 0, "T" : 0} for index in range(M)}
+
+for _ in range(N):
+    input_dna = input()
+    for idx, item in enumerate(input_dna):
+        pre_define_counter[idx][item] += 1
+
+answer_dna = ""
+answer_hamming_value = 0
+for key, value in pre_define_counter.items():
+    large_char = max(value, key=value.__getitem__)
+    value.pop(large_char)
+    answer_dna += large_char
+
+    for char, count in value.items():
+        answer_hamming_value += count
+
+print(pre_define_counter)
+
+print(answer_dna)
+print(answer_hamming_value)
